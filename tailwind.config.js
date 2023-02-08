b0324a8bc435754f6271f661ab41cfc8e7253cba
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-08 20:42:04 +0200

    own border-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main/resources/templates/**/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/main/resources/templates/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                'thymeleaf-green': 'darkseagreen',
+                'thymeleaf-blue': '#1fb6ff',
+                'thymeleaf-purple': '#7e5bef',
+                'thymeleaf-pink': '#ff49db',
+                'thymeleaf-orange': '#ff7849',
+                'thymeleaf-greens': '#13ce66',
+                'thymeleaf-yellow': '#ffc82c',
+                'thymeleaf-gray-dark': '#273444',
+                'thymeleaf-gray': '#8492a6',
+                'thymeleaf-gray-light': '#d3dce6',
+            },
+            fontFamily: {
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            }
+        },
+    },
+    plugins: [],
 }
